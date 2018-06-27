1c2ce88731e1bb97a8b347f1291314071e1e70c4
Nairi Harutyunyan <nairihar99@gmail.com>
2018-06-27 23:41:42 +0400

    change simple test

--- test/index.js
+++ test/index.js
@@ -3,5 +3,5 @@ const assert = require('assert');
 const utils = require('../spec/utils');
 
 it('test', () => {
-  assert.equal(utils.testSum(1, 2), 3);
+  assert.equal(utils.testSum(1, 1), 2);
 });
